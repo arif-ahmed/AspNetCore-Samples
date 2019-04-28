3897eb94b77cb9ecfffe958d7b9aaee997ac1ca5
arif-ahmed <khandaker.arif.ahmed@gmail.com>
2019-04-29 00:43:36 +0600

    Updated

--- Error-Handling/ErrorHandling/Controllers/HomeController.cs
+++ Error-Handling/ErrorHandling/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ErrorHandling.Filters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,10 +7,13 @@ namespace ErrorHandling.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        [HandleException]
+        //[HandleException(ViewName = "Error",
+        //ExceptionType = typeof(DivideByZeroException))]
+        //[HandleException(ViewName = "Error")]
         public IActionResult Index()
         {
-            throw new Exception();
+            throw new Exception("This is some exception!!!");
             //return View();
         }
     }
---
+++ Error-Handling/ErrorHandling/Filters/HandleExceptionAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErrorHandling.Filters
+{
+    public class HandleExceptionAttribute : ExceptionFilterAttribute
+    {
+        public string ViewName { get; set; } = "Error";
+        public Type ExceptionType { get; set; } = null;
+        public override void OnException(ExceptionContext context)
+        {
+
+            if(ExceptionType != null)
+            {
+                if (context.Exception.GetType() == this.ExceptionType)
+                {
+                    // do things accordingly
+                }
+
+                var result = new ViewResult { ViewName = this.ViewName };
+                var modelMetadata = new EmptyModelMetadataProvider();
+                result.ViewData = new ViewDataDictionary
+                (modelMetadata, context.ModelState);
+                result.ViewData.Add("HandleException", context.Exception);
+                context.Result = result;
+                context.ExceptionHandled = true;
+            }
+            else
+            {
+                var result = new ViewResult { ViewName = "Error" };
+                var modelMetadata = new EmptyModelMetadataProvider();
+                result.ViewData = new ViewDataDictionary
+                (modelMetadata, context.ModelState);
+                result.ViewData.Add("HandleException", context.Exception);
+                context.Result = result;
+                context.ExceptionHandled = true;
+            }
+
+        }
+    }
+}
+
