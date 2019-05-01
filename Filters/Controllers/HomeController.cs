4c8dc35394dad6e3a306f662adcca21d853508f6
arif-ahmed <khandaker.arif.ahmed@gmail.com>
2019-05-02 00:59:50 +0600

    Filters Sample Added

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AboutUs()
+        {
+            return Content("About Us Page");
+            //return View();
+        }
+    }
+}
---
+++ Filters/Filters/ResourceFilters/IEFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Filters.Filters.ResourceFilters
+{
+    public class IEFilterAttribute : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            string userAgent = context.HttpContext.Request.Headers["User-Agent"].ToString();
+            if (Regex.IsMatch(userAgent, "MSIE [6-9]") || userAgent.Contains("Trident"))
+            {
+                context.Result = new ContentResult { Content = "Your browser is not supported" };
+            }
+        }
+    }
+}
